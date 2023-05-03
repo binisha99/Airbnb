4a91de8e432f2af8a457cd103b379e9ce9ed2faf
binisha <binishabajracharya708@gmail.com>
2023-05-03 11:22:09 +0545

    used props

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,70 +1,24 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
       <article>
-        <img src="./images/image 12.png" alt="katie" className="card_image" />
+        <img src={`../images/${props.img}`} className="card_image" />
+      </article>
+      <section>
         <div className="card--stats">
-        <img src="./images/Star 1.png" />
-        <span>5.0</span>
-        <span className="grey">(6).</span>
-        <span className="grey">USA</span>
+          <img src={props.img2} />
+          <span>{props.rating}</span>
+          <span className="grey">{props.rate}</span>
+          <span className="grey">{props.country}</span>
         </div>
+        <p>{props.quote}</p>
         <p>
-        Life lessons with Katie Zaferes
+          <span className="bold">{props.bold}</span>
+          {props.person}
         </p>
-        <p>
-            <span className="bold">
-            From $136
-        </span>
-         / person
-        </p>
-      </article>
-      <article>
-        <img
-          src="./images/wedding-photography 1.png"
-          alt="wedding photography"
-          className="card_image"
-        />
-            <div className="card--stats">
-        <img src="./images/Star 1.png" />
-        <span>5.0</span>
-        <span className="grey">(30).</span>
-        <span className="grey">USA</span>
-        </div>
-        <p>
-        Learn wedding photography
-        </p>
-        <p>
-       <span className="bold">
-       From $125
-        </span>  / person
-        </p>
-      </article>
-      <article>
-        <img
-          src="./images/mountain-bike 1.png"
-          alt="mountain-bike"
-          className="card_image"
-        />
-           <div className="card--stats">
-        <img src="./images/Star 1.png" />
-        <span>4.8</span>
-        <span className="grey">(2).</span>
-        <span className="grey">USA</span>
-        </div>
-        <p>
-        Group Mountain Biking
-        </p>
-        <p>
-            <span className="bold">
-            From $50 
-            </span>
-       / person
-
-        </p>
-      </article>
+      </section>
     </div>
   );
 }
